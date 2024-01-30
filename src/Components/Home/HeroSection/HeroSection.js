b2b48c182604168e5bfc345a29422408ed6aaed1
Daniel <daniel.a.valarmathi@gmail.com>
2024-01-30 16:33:44 +0530

    Herosection content changes

--- src/Components/Home/HeroSection/HeroSection.js
+++ src/Components/Home/HeroSection/HeroSection.js
@@ -7,9 +7,9 @@ const HeroSection = () => {
         <div className="overlay"></div>
         <video src="Hero.mp4" autoPlay loop muted />
         <div className="content">
-            <h1>Welcome</h1>
+            {/* <h1>Welcome</h1>
             <h2>TO</h2>
-            <h1>IDA</h1>
+            <h1>IDA</h1> */}
          
         </div>
     </div>
